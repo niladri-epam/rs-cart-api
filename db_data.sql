bc73ba2833a4f9a0d2a4690a9a856a91c1995cd2
Niladri Mahato <niladrimahato@Niladris-MacBook-Air.local>
2023-11-26 20:42:12 +0530

    added more data to carts

--- db_data.sql
+++ db_data.sql
@@ -1,2 +1,5 @@
 insert into carts (user_id, created_at, updated_at, status) values ('5a16ae03-db21-4e14-b0fc-38500daffa38', '2023-11-26', '2023-11-26', 'OPEN');
+insert into carts (user_id, created_at, updated_at, status) values ('cafc28af-2b8c-482b-ba06-e0350251c99c', '2023-11-26', '2023-11-26', 'ORDERED');
+insert into carts (user_id, created_at, updated_at, status) values ('569ff367-608a-47a9-959d-6cb398869320', '2023-11-26', '2023-11-26', 'ORDERED');
+
 insert into cart_items (cart_id, product_id, count) values ('fa64c84d-6b96-4235-8f6f-68960134e7bb', '5a16ae03-db21-4e14-b0fc-38500daffa38', 4);
