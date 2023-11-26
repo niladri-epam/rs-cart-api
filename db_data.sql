c949d7ffc2de1ad5aab7203d7e4e37ff28ab25a5
Niladri Mahato <niladrimahato@Niladris-MacBook-Air.local>
2023-11-26 20:27:16 +0530

    task 8 completed

---
+++ db_data.sql
@@ -0,0 +1,2 @@
+insert into carts (user_id, created_at, updated_at, status) values ('5a16ae03-db21-4e14-b0fc-38500daffa38', '2023-11-26', '2023-11-26', 'OPEN');
+insert into cart_items (cart_id, product_id, count) values ('fa64c84d-6b96-4235-8f6f-68960134e7bb', '5a16ae03-db21-4e14-b0fc-38500daffa38', 4);
